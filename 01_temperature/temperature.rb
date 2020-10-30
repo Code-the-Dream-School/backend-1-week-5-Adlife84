796740b08813773db787305d893de2899ce32a43
Andrei <andreykomolov@gmail.com>
2020-10-30 17:20:24 -0400

    Done with 02_calculator

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,12 @@
-#write your code here
+#write your code here Formula	
+#(32°F − 32) × 5/9 = 0°C
+
+# Conver to celsius
+def ftoc(fahrinheit)
+    return (fahrinheit - 32) * 5/9
+end
+
+# Convert to fahrinheit
+def ctof(celsius = 37)
+    return (celsius.to_f * 9/5) + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,17 @@
 #write your code here
+def add(a, b)
+    return a + b
+end
+
+def subtract(a, b)
+    return a - b
+end
+
+def sum(array)
+    sum = 0
+    array.each do |i| 
+        sum += i 
+    end
+    return sum
+end
+
