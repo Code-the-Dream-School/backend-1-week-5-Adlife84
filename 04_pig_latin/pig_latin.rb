f7c41d19ba8e0f3e8c2162765b7ed42201c67f00
Andrei <andreykomolov@gmail.com>
2020-10-31 16:42:44 -0400

    Working on 04_pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,25 @@
 #write your code here
+def translate(string = "apple banana cherry")
+    wordsArray = string.split
+    vowelArray = ["a","e","i","o","u"]
+
+    wordsArray.each_with_index do |word, index|
+        puts word
+        vowelArray.each do |vowel|
+            if word[0] != vowel && word[1] != vowel
+                wordsArray[index] = word[2, word.length] + word[0,2] + "ay"
+                break
+            elsif word[0] != vowel
+                wordsArray[index] = word[1, word.length] + word[0] + "ay"
+                break
+            elsif word[0] == vowel 
+                wordsArray[index] = word + "ay"
+                break
+            end
+        end
+    end
+
+    return wordsArray.join(" ")
+end
+
+translate()
